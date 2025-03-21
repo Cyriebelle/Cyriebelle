8841979c45145f1461a9430a10cb2087ea1e488b
Cyriebelle <mariahcyriebelle.jurao@dcmail.ca>
2025-03-21 16:42:53 -0400

    FIXING THE CODE
    Made some changes in the JavaScript.

--- Assignment 4/PART 1/main.js
+++ Assignment 4/PART 1/main.js
@@ -19,21 +19,18 @@ const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
+randomize.addEventListener('click', result);
+
 function result() {
     let newStory =  storyText;
     
-    console.log(
-      `Placeholder story: 
-      ${newstory}`)
-
     const xItem = randomValueFromArray(insertX);
     const yItem = randomValueFromArray(insertY);
     const zItem = randomValueFromArray(insertZ);
 
-    newStory = newStory
-                .replaceAll(":insertx:", xItem)
-                .replace("inserty:", yItem)
-                .replace("insertz:", zItem);
+    newStory = newStory.replaceAll(":insertx:", xItem)
+    newStory = newStory.replaceAll("inserty:", yItem)
+    newStory = newStory.replaceAll("insertz:", zItem);
 
     if(customName.value !== '') {
       const name = customName.value;
@@ -41,12 +38,13 @@ function result() {
   
     }
   
-    if(document.getElementById("uk").checked) {
-      const weight = Math.round(300);
-      const temperature =  Math.round(94);
-  
+    if(ukChecked === true) {
+      const weight = `${Math.round(300 / 14)} stone`;
+      const temperature =  `${Math.round((94 -32) * (5/9))} centigrade`;
+      newStory = newStory.replace("300 pounds", weight)
+      newStory = newStory.replace("94 fahrenheit", temperature)
     }
   
-    story.textContent = ;
+    story.textContent = newStory;
     story.style.visibility = 'visible';
   }
--- Assignment 4/PART 1/test.js
+++ Assignment 4/PART 1/test.js
@@ -1,7 +1,11 @@
-const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const storyText = "It was 94 [fahrenheit] outside, so [:insertx:] went for a walk. When they got to [:inserty:], they stared in horror for a few moments, then [:insertz:]. [Bob] saw the whole thing, but was not surprised — [:insertx:] weighs 300 [pounds], and it was a hot day.";
 const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+const customName = "Dr.Seuss";
+
+const ukChecked = true;
 
 function result() {
   let newStory = storyText
@@ -14,6 +18,15 @@ function result() {
               .replaceAll(":insertx:", xItem)
               .replace("inserty:", yItem)
               .replace("insertz:", zItem);
+
+    if(ukChecked === true) {
+      const weight = `${Math.round(300 / 14)} stone`;
+      const temperature =  `${Math.round((94 -32) * (5/9))} centigrade`;
+      newStory = newStory
+                  .replace("300 pounds", weight)
+                  .replace("94 fahrenheit", temperature)
+    }
+
 
   return `New story:
               ${newStory}`;
