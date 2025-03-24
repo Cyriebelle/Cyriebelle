5361f61d75bfd8fd16afd05d57daff646c0f040f
Cyriebelle <mariahcyriebelle.jurao@dcmail.ca>
2025-03-24 17:52:51 -0400

    FIXING THE BUTTON PART

--- Assignment 4/PART 2/gallery-start/gallery-start/main.js
+++ Assignment 4/PART 2/gallery-start/gallery-start/main.js
@@ -1,44 +1,44 @@
+/* Selecting the HTML elements */
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imgArr = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
+const imgArr = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
 const imgAlts = [
-    {altText: "Photo 1- Mountain Trail Path"},
+    {altText: "Photo 1 - Mountain Trail Path"},
     {altText: "Photo 2 - Mountain Peak and Trail"},
     {altText: "Photo 3 - Orange Flowers"},
     {altText: "Photo 4 - Desert Mountain View"},
-    {altText: "Photo 5 - Sunrise in Dessert"}
+    {altText: "Photo 5 - Sunrise in the Desert"}
+  ];
 
-]
 /* Looping through images */
-for (let i = 0; i < imageArray.length; i++) {
+for (let i = 0; i < imgArr.length; i++) {
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', 'images/' +imageArray[i]);
-    newImage.setAttribute('alt', imgAlts[i].altText)
-    thumbBar.appendChild(newImage)
-    
+    newImage.setAttribute('src', `images/${imgArr[i]}`);
+    newImage.setAttribute('alt', imgAlts[i].altText);
+    thumbBar.appendChild(newImage);
 }
 
+/* Set the src and alt attributes on the large image when a thumbnail is clicked */
 thumbBar.addEventListener("click", (event) => {
     displayedImage.setAttribute('src', event.target.getAttribute('src'));
     displayedImage.setAttribute('alt', event.target.getAttribute('alt'));
-})
+});
 
 /* Wiring up the Darken/Lighten button */
 btn.addEventListener("click", () => {
-    const currentClass = btn.getAttribute("class");
-    if (currentClass === "dark") {
+    if (btn.getAttribute("class") === "dark") {
         btn.setAttribute("class", "light");
         btn.textContent = "Lighten";
-        overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)"; // Dark overlay
+        overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
     } else {
         btn.setAttribute("class", "dark");
         btn.textContent = "Darken";
-        overlay.style.backgroundColor = "rgba(0, 0, 0, 0)"; // Transparent overlay
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
     }
 });
