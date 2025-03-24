4bf35fc4914cd63c21f32a0909f9a97fb6401d72
Cyriebelle <mariahcyriebelle.jurao@dcmail.ca>
2025-03-24 16:46:21 -0400

    JAVASCRIPT CHANGES

--- Assignment 4/PART 2/gallery-start/gallery-start/main.js
+++ Assignment 4/PART 2/gallery-start/gallery-start/main.js
@@ -7,7 +7,14 @@ const overlay = document.querySelector('.overlay');
 const imgArr = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
 
 /* Declaring the alternative text for each image file */
+const imgAlts = [
+    {altText: "Photo 1- Mountain Trail Path"},
+    {altText: "Photo 2 - Mountain Peak and Trail"},
+    {altText: "Photo 3 - Orange Flowers"},
+    {altText: "Photo 4 - Desert Mountain View"},
+    {altText: "Photo 5 - Sunrise in Dessert"}
 
+]
 /* Looping through images */
 
 const newImage = document.createElement('img');
