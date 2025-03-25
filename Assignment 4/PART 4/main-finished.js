c9adecf112e3871371b8ea36c304976395ebf946
Cyriebelle <mariahcyriebelle.jurao@dcmail.ca>
2025-03-25 12:23:58 -0400

    DELETED SOME PARTS IN JAVASCRIPT.

--- Assignment 4/PART 4/main-finished.js
+++ Assignment 4/PART 4/main-finished.js
@@ -102,15 +102,4 @@ function loop() {
   requestAnimationFrame(loop);
 }
 
-
-class Triangle {
-    constructor(x, y, velX, velY, color, size) {
-      this.x = x;
-      this.y = y;
-      this.velX = velX;
-      this.velY = velY;
-      this.color = color;
-      this.size = size;
-    }
-}
 loop();
